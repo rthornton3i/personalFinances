4f544ac7d6d6358c42e3c582cc9da8521ed41cae
rthornton3i <rthorntoniii@gmail.com>
2024-01-07 14:54:34 -0500

    Update _main.py
    cleanup main run

--- python/_main.py
+++ python/_main.py
@@ -33,32 +33,38 @@ class Main:
 
         loans = Loans(self.vars)
         self.vars = loans.run()
+        """
+        Add refinancing
+        Add loan forgiveness and income driven plan
+        """
 
         self.toc = time()
         print('Initial setup done: ',str(self.toc-self.tic))
 
-        self.executeSingle()
-        # self.executeMulti()
+        # self.executeSingle()
+        self.executeMulti()
 
         self.toc = time()
         print('Done: ',str(self.toc-self.tic))
 
     def executeSingle(self):
-        outputs = self.loop()
-        self.totalSavings = []
-        for t in ['totalSavings','totalExpenses']:
-            total = pd.DataFrame()
-            for i,output in enumerate(outputs[t]):
-                self.totalSavings.append(np.sum(output.iloc[-1,:]))
-                if i == 0:
-                    total = output
-                else:
-                    total += output
+        def getTotal(summary):
+            total = 0
+            endVal = []
+            for i,val in enumerate(summary):
+                endVal.append(np.sum(val.iloc[-1,:]))
 
-            if t == 'totalSavings':
-                self.avgTotalSavings = total / (self.vars.base.loops / mp.cpu_count())
-            else:
-                self.avgTotalExpenses = total / (self.vars.base.loops / mp.cpu_count()) 
+                if i == 0:
+                    total = val
+                else:
+                    total += val
+
+            return total,endVal
+        
+        outputs = self.loop()
+        
+        total,self.totalSavings = getTotal(outputs['totalSavings'])
+        self.avgTotalSavings = total / (self.vars.base.loops / mp.cpu_count())
 
     def executeMulti(self):
         def getTotal(summary):
@@ -74,8 +80,8 @@ class Main:
 
             return total,endVal
         
-        totalSavings = []
-        totalExpenses = []
+        self.allSavings = []
+        self.allExpenses = []
         with Pool(processes=mp.cpu_count()) as pool:
             results = [pool.apply_async(self.loop) for _ in range(mp.cpu_count())]
 
@@ -85,12 +91,12 @@ class Main:
             [r.wait() for r in results]
             for r in results:
                 result = r.get()
-                totalSavings.extend(result['totalSavings'])
-                totalExpenses.extend(result['totalExpenses'])
+                self.allSavings.extend(result['totalSavings'])
+                self.allExpenses.extend(result['totalExpenses'])
 
-        total,self.totalSavings = getTotal(totalSavings)
+        total,self.totalSavings = getTotal(self.allSavings)
         self.avgTotalSavings = total / self.vars.base.loops
-        total,_ = getTotal(totalExpenses)
+        total,_ = getTotal(self.allExpenses)
         self.avgTotalExpenses = total / self.vars.base.loops
 
         self.avgTotalSavings.to_csv('Outputs/Savings.csv')      
@@ -120,9 +126,11 @@ class Main:
             """
             Add Medicare expenses to health
             Account for std deduction when retired
+            Only tax earnings in traditional 401
+            Add underflow/overflow
             """
 
-            totalSavings.append(self.vars.savings.savings)
+            totalSavings.append(self.vars.taxes.savings)
             totalExpenses.append(self.vars.expenses.totalExpenses)
 
             print('Loop ',str(i+1),'/',str(loops),' done: ',str(time()-self.toc))
@@ -133,7 +141,7 @@ class Main:
 
         return outputs
 
-if __name__ == '__main__':
+def run():
     import pickle
 
     main = Main()
@@ -144,15 +152,31 @@ if __name__ == '__main__':
     # with open('Inputs/main.pkl', 'rb') as file:
     #     main = pickle.load(file)
 
-    print(f'{np.sum(main.avgTotalSavings.iloc[-1,:]):,.0f}')
+    print(f'{np.sum(main.avgTotalSavings.iloc[-1,:])/main.vars.salary.summedInflation[-1]:,.0f}')
 
     plt.figure()
     n = len(main.avgTotalSavings.columns)
     plt.plot(main.avgTotalSavings.iloc[:,-n:])    
     plt.legend(main.avgTotalSavings.columns[-n:])
     plt.plot(np.zeros((main.vars.base.years,1)),'--')
-    plt.show()
+    # plt.show()
 
     plt.figure()
-    plt.hist(main.totalSavings,bins=max(1,int(main.vars.base.loops/20)),rwidth=0.9)
+    std = np.std(main.totalSavings)
+    avg = np.mean(main.totalSavings)
+    plt.hist(main.totalSavings/main.vars.salary.summedInflation[-1],
+             bins=max(1,int(main.vars.base.loops/20)),
+             rwidth=0.9,
+             range=(avg-2*std,avg+3*std)/main.vars.salary.summedInflation[-1])
     plt.show()
+
+    # plt.figure()
+    # accounts = main.vars.accounts
+    # accountType = accounts.accountSummary.accountType
+    # for acc,accType in accountType.items():
+    #     main.allSavings
+    #     plt.hist()
+
+
+if __name__ == '__main__':
+    run()
